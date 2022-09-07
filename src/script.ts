bebad18c6d3eb0ffd8b8a9fb3139401d9f8133e5
Maksim Shyla <maksim.shila@gmail.com>
2022-09-07 17:44:59 +0300

    Added sample

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,21 @@
 window.addEventListener("load", () => {
-    const arr = [":asdasd", "asdasd"];
+    const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+    canvas.width = 600;
+    canvas.height = 600;
+
+    const context = canvas.getContext("2d")!;
+
+    let x = 0;
+    let speed = 10;
+    function animate() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillRect(x, 50, 100, 100);
+        x += speed;
+        if (x >= canvas.width - 100 || x <= 0) {
+            speed = -speed;
+        }
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
