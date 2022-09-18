01c0cf959aff3f7b89d2f1a5c4c6abdbe39a8567
Maksim Shyla <maksim.shila@gmail.com>
2022-09-18 17:35:42 +0300

    Add menu select/change sounds

--- src/menu/menuList.ts
+++ src/menu/menuList.ts
@@ -3,11 +3,18 @@ import InputHandler from "../input.js";
 export default class MenuList {
 
     private readonly menuButtons: HTMLButtonElement[];
+    private readonly changeSound: HTMLAudioElement;
+    private readonly selectSound: HTMLAudioElement;
     private activeBtn: HTMLButtonElement | null = null;
     private defaultBtn: HTMLButtonElement;
 
     constructor(menuButtons: HTMLButtonElement[], defaultBtn: HTMLButtonElement) {
         this.menuButtons = menuButtons;
+        this.menuButtons.forEach(btn => btn.addEventListener("click", () => this.playSelectSound()));
+        this.changeSound = new Audio();
+        this.changeSound.src = "assets/sounds/menu_change.mp3";
+        this.selectSound = new Audio();
+        this.selectSound.src = "assets/sounds/menu_select.mp3";
         this.defaultBtn = defaultBtn;
         this.setActive(this.defaultBtn);
     }
@@ -42,6 +49,23 @@ export default class MenuList {
                 : activeBtnIndex === 0 ? this.menuButtons.length - 1 : activeBtnIndex - 1;
             const activeBtn = this.menuButtons[newIndex];
             this.setActive(activeBtn);
+            this.playChangeSound();
         }
+    }
+
+    private playChangeSound(): void {
+        if (!this.changeSound.paused) {
+            this.changeSound.pause();
+            this.changeSound.currentTime = 0;
+        }
+        this.changeSound.play();
+    }
+
+    private playSelectSound(): void {
+        if (!this.selectSound.paused) {
+            this.selectSound.pause();
+            this.selectSound.currentTime = 0;
+        }
+        this.selectSound.play();
     }
 }
